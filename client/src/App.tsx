d184bbfde901c4e5fab04ad1997176b0b7bb7d5a
Joe Carré <carre.joe3@gmail.com>
2022-08-03 14:48:20 +0100

    started waveform component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RecordButton } from './components/RecordButton';
+import { Waveform } from './components/Waveform';
 import './App.css';
 import '../node_modules/papercss/dist/paper.min.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <div className="paper container">
         <RecordButton />
+        <Waveform />
       </div>
     </div>
   );
---
+++ client/src/components/Waveform.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import WaveSurfer from 'wavesurfer.js'
+// import * as MicrophonePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.microphone'
+
+export const Waveform = (): React.ReactElement => {
+  const [ waveformBuilt, setWaveformBuilt ] = useState(false);
+  const [ waveForm, setWaveForm ] = useState<null | WaveSurfer>(null)
+
+  const buildWaveform = (): void => {
+    setWaveForm(WaveSurfer.create({
+      container: '#waveform',
+      waveColor: '#D2EDD4',
+      barHeight: 20,
+      hideScrollbar: true,
+      audioRate: 1,
+      barWidth: 2,
+      interact: false,
+      cursorWidth: 0,
+      height: 200,
+      normalize: true
+      // plugins: [
+      //   MicrophonePlugin.create()
+      // ]
+    }));
+    setWaveformBuilt(true);
+  }
+
+  useEffect(() => {
+    buildWaveform();
+  }, [])
+
+  return (
+    <div id="waveform"></div>
+  )
+}
